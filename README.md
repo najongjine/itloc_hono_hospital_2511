675e7d0d8853bb9c96df7eae2ffa6c4e607a62a3
najongjine <najongjin3@hotmail.com>
2025-12-10 09:53:00 +0900

    .

--- README.md
+++ README.md
@@ -5,8 +5,11 @@ npm run dev
 
 npm i cross-env
 
+npm i dotenv
+
 package.json
 -scripts.dev 를
+
 cross-env NODE_ENV=development tsx watch src/index.ts
 
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import * as dotenv from "dotenv";
+
+const envFile =
+  process.env.NODE_ENV === "production"
+    ? ".env.production"
+    : ".env.development";
+dotenv.config({ path: envFile });
 
 const app = new Hono();
 /**
