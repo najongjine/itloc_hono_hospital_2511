731491684c209f7e5b544a1b2390e918a931e6c9
najongjine <najongjin3@hotmail.com>
2025-12-19 11:48:58 +0900

    .

--- src/router/hospital_router.ts
+++ src/router/hospital_router.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
+import { HonoEnv } from "../types/types.js";
 
-const router = new Hono();
+const router = new Hono<HonoEnv>();
 
 interface ResultType {
   success?: boolean;
@@ -10,6 +11,7 @@ interface ResultType {
 router.get("/", async (c) => {
   let result: ResultType = { success: true };
   try {
+    const db = c.var.db;
     const kakao_restapi_key = process.env.KAKAO_RESTAPI_KEY;
 
     let query = String(c?.req?.query("query") ?? "병원");
@@ -37,9 +39,37 @@ router.get("/", async (c) => {
       }
     );
 
-    let _data = await response.json();
-    console.log(`_data: `, _data);
-    result.data = _data?.documents ?? [];
+    let _data: any = await response.json();
+    _data = _data?.documents ?? [];
+    const ids: string[] = _data.map((item: any) => item.id);
+
+    let _data2: any = await db.query(
+      "SELECT * FROM hospitals WHERE id = ANY($1)",
+      [ids] // 배열 전체를 하나의 파라미터($1)로 전달
+    );
+    _data2 = _data2?.rows ?? [];
+
+    const hospitalMap: any = new Map(
+      _data2.map((item: any) => [item.id, item])
+    );
+
+    // 2. _data를 순회하며 데이터 병합 및 가공
+    _data = _data.map((item: any) => {
+      // 매칭되는 DB 데이터 찾기
+      const dbItem: any = hospitalMap.get(item?.id);
+
+      return {
+        ...item, // 기존 _data의 필드 유지
+
+        // 요청 1: distance 복사해서 distance_m 만들기
+        distance_m: item.distance,
+
+        // 요청 2: 매칭 데이터가 있으면 그 값을, 없으면 기본값(3, 2) 사용
+        rating: dbItem ? dbItem?.rating : 3,
+        congestion_level: dbItem ? dbItem.congestion_level : 2,
+      };
+    });
+    result.data = _data2;
     return c.json(result);
   } catch (error: any) {
     result.success = false;
